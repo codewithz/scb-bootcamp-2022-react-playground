997a6d79c78ec560b7a14e44b2607efad69cdab9
Zartab Nakhwa <zartab@codewithz.com>
2022-07-20 15:20:00 +0530

    Customer UC- Customer Form State

--- src/components/UseCaseThree/CustomerForm.jsx
+++ src/components/UseCaseThree/CustomerForm.jsx
@@ -1,4 +1,53 @@
+import { useState } from "react"
+
 export function CustomerForm() {
+
+    const blankCustomer = { id: 0, firstName: '', lastName: '', email: '' }
+
+    const [customer, setCustomer] = useState(blankCustomer)
+
+    // Event Handling
+    // const handleFirstName = (event) => {
+    //     console.log(event.target.name)
+    //     console.log(event.target.value)
+
+    //     let customerClone = { ...customer }
+    //     customerClone["firstName"] = event.target.value
+
+    //     setCustomer(customerClone)
+    // }
+    // const handleLastName = (event) => {
+    //     console.log(event.target.name)
+    //     console.log(event.target.value)
+
+    //     let customerClone = { ...customer }
+    //     customerClone["lastName"] = event.target.value
+
+    //     setCustomer(customerClone)
+    // }
+    // const handleEmail = (event) => {
+    //     console.log(event.target.name)
+    //     console.log(event.target.value)
+
+    //     let customerClone = { ...customer }
+    //     customerClone["email"] = event.target.value
+
+    //     setCustomer(customerClone)
+    // }
+
+    // -------------- Refactoring -----------------------
+
+    const handleInput = (event) => {
+        // console.log(event.target.name)
+        // console.log(event.target.value)
+
+        const { name, value } = event.target; // Destructuring name and value from event.target
+        let customerClone = { ...customer }
+        customerClone[name] = value;
+
+        setCustomer(customerClone)
+    }
+
     return (
         <div>
             <p className="lead">Add Customer</p>
@@ -7,17 +56,19 @@ export function CustomerForm() {
                 <div className="form-group">
                     <input
                         type="text"
-                        name="first-name"
+                        name="firstName"
                         className="form-control"
                         placeholder="Enter First Name"
+                        onChange={handleInput}
                     />
                 </div>
                 <div className="form-group">
                     <input
                         type="text"
-                        name="last-name"
+                        name="lastName"
                         className="form-control"
                         placeholder="Enter Last Name"
+                        onChange={handleInput}
                     />
                 </div>
 
@@ -28,6 +79,7 @@ export function CustomerForm() {
                         name="email"
                         className="form-control"
                         placeholder="Enter Email"
+                        onChange={handleInput}
                     />
                 </div>
                 <button className="btn btn-primary btn-sm m-2">
