e7d4c2a7d179bca84e3a99bbdad2fc7e90551989
Zartab Nakhwa <zartab@codewithz.com>
2022-07-21 11:09:15 +0530

    Routing- Consuming Route Parameters

--- src/components/RoutingApp/ProductDetails.jsx
+++ src/components/RoutingApp/ProductDetails.jsx
@@ -1,7 +1,14 @@
-export function ProductDetails() {
+import { useParams } from "react-router-dom"
+
+export function ProductDetails(props) {
+
+    console.log(props.match.params.id)
+
+    const { id } = useParams();
+
     return (
         <div>
-            <h2>Product Details - </h2>
+            <h2>Product Details - {id}</h2>
             <button className="btn-warning btn-sm m-2">
                 Save
             </button>
