0ccaf5a76f363b93013f367a4306d3e3668055e8
Zartab Nakhwa <zartab@codewithz.com>
2022-07-21 11:29:34 +0530

    Routing- Programmatic Navigation

--- src/components/RoutingApp/ProductDetails.jsx
+++ src/components/RoutingApp/ProductDetails.jsx
@@ -1,15 +1,24 @@
-import { useParams } from "react-router-dom"
+import { useParams, useHistory } from "react-router-dom"
 
 export function ProductDetails(props) {
 
+    const history = useHistory();
     console.log(props.match.params.id)
 
     const { id } = useParams();
 
+    const handleSave = () => {
+        //OPerations to save the product
+        // Navigate to /products
+        history.push("/products")
+    }
+
     return (
         <div>
             <h2>Product Details - {id}</h2>
-            <button className="btn-warning btn-sm m-2">
+            <button
+                onClick={handleSave}
+                className="btn-warning btn-sm m-2">
                 Save
             </button>
         </div>
