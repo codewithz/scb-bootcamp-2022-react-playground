d99251a580a90c698242bea277b87b4a609c8d1f
Zartab Nakhwa <zartab@codewithz.com>
2022-07-21 11:22:02 +0530

    Routing- Redirection

--- src/components/RoutingPath.jsx
+++ src/components/RoutingPath.jsx
@@ -1,9 +1,10 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { Products } from './RoutingApp/Products';
 import { Posts } from './RoutingApp/Posts';
 import { AdminDashboard } from './RoutingApp/admin/AdminDashboard';
 import { Home } from './RoutingApp/Home';
 import { ProductDetails } from './RoutingApp/ProductDetails';
+import { NotFound } from './RoutingApp/NotFound';
 
 export function RoutingPath() {
 
@@ -14,7 +15,9 @@ export function RoutingPath() {
                 <Route path="/products" component={Products} />
                 <Route path="/posts/:year/:month?" component={Posts} />
                 <Route path="/admin-dashboard" component={AdminDashboard} />
+                <Route path="/not-found" component={NotFound} />
                 <Route path="/" component={Home} exact />
+                <Redirect to="/not-found" />
             </Switch>
         </div>
     )
